29dad62d023106fff378a412a322ef83a033c36a
MaitreWaff <8455859+MaitreWaff@users.noreply.github.com>
2026-01-14 22:12:57 +0100

    Adding conditionals to ZSH setup file

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -4,11 +4,31 @@ echo "\n<<< Starting ZSH Setup >>>\n"
 
 # Installation unnecessary; it's in the Brewfile.
 
-echo "Enter superuser (sudo) password to edit /etc/shells"
-echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' > /dev/null
+if grep -Fxq '/opt/homebrew/bin/zsh' '/etc/shells'; then
+  echo '/opt/homebrew/bin/zsh already exists in /etc/shells'
+else
+  echo "Enter superuser (sudo) password to edit /etc/shells"
+  #echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' > /dev/null
+  echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' > /dev/null
+fi
 
-chsh -s '/usr/local/bin/zsh'
+if [ "$SHELL" = '/opt/homebrew/bin/zsh' ]; then
+  echo '$SHELL is already /opt/homebrew/bin/zsh'
+else
+  echo "Enter superuser (sudo) password to edit /etc/shells"
+  chsh -s '/opt/homebrew/bin/zsh'
+fi
 
-ln -s /usr/local/bin/zsh /private/var/select/sh
+if sh --version | grep -q zsh; then
+  echo '/private/var/select/sh already linked to /bin/zsh'
+else
+  echo "Enter superuser (sudo) password to symlink sh to zsh"
+  # Looked cute, might delete later, idk
+  #ln -s /usr/local/bin/zsh /private/var/select/sh
+  sudo ln -sfv /bin/zsh /private/var/select/sh
+
+  # I'd like for this to work instead.
+  # sudo ln -sfv /opt/homebrew/bin/zsh /private/var/select/sh
+fi
 
 echo "\n<<< ZSH Setup Done!!!. >>>\n"
